07b6b259c5a7ea681d31d475425a11581657c51a
Roberto <rrferro07@gmail.com>
2024-06-22 20:49:43 -0400

    3rd commit. addingg JSON with  list of dictonaries that contain questions with python as a tag

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import requests
 import json
+import os
 
 response = requests.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
 #print(type(response))
 
 #creating a new empty dict
-new_dict = {}
+global new_dict
+new_dict = {"questions": []}
+
+global count
+count = 0
 
 for data in response.json()['items']:
-    res = next((sub for sub in data["tags"] if sub == "python"), None)
-    if (res == "python"):       
-        new_dict.update({"Title": data['title'], "Link": data['link'], "View Count": data['view_count'] , "answered": data['is_answered']})
-       # print(f"{data['title']}, {data['link']}, {data['is_answered']}, {data['view_count']}")
-        #print(data["link"]) working just for link
-        #print(type(res))
+    #print(data["tags"])
+    res = next((sub for sub in data["tags"] if sub == "python"), None)   
+    #print(res) 
+    if (res == "python"):
+        count = count + 1       
+        new_dict["questions"].append({'Title': data['title'], 'Link': data['link'], 'View Count': data['view_count'] , 'answered': data['is_answered']})
+        #print(f"{data['title']}, {data['link']}, {data['is_answered']}, {data['view_count']}")
         #print(f"res variable is: {res}")
         #print(type(data)) # this is a dict
-        #print(type(data["tags"])) # this is a list
-        
-        
-print(type(new_dict))
-print(new_dict)
-        
-        #add elements to a dictionary
-        
+        #print(type(data["tags"])) # this is a list   
+#print(new_dict)
+#print(count)
+      
+        # Serializing json
+json_object = json.dumps(new_dict, indent=4)
+ 
+# Writing to questions.json
+with open(r"%s\questions.json" % os.path.dirname(__file__), "w") as outfile:
+    outfile.write(json_object)
+
+#Printing the current working directory
+#print(os.getcwd())
+
